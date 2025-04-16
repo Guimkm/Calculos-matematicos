9e37c7734ef8d1a7bcf1da7317db2b4f103a6319
Guilherme Araujo Carneiro da Silva <guilhermearaujocarneiro26@gmail.com>
2025-04-16 15:06:20 -0300

    Add files via upload

--- CalculosMatematicos.java
+++ CalculosMatematicos.java
@@ -8,13 +8,31 @@ package com.mycompany.calculosmatematicos;
  *
  * @author FATEC ZONA LESTE
  */
-public class CalculosMatematicos {
+  // Eu Guilherme Araujo Carneiro Da Silva desenvolvi esses calculos matemáticos
 
-    /**
-     * @param args the command line arguments
-     */
+public class CalculosMatematicos {
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Definição de dois numeros
+        double num1 = 10.75;
+        double num2 = 14.60;
+        
+        // Realização das operações matematicas
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num1 * num2;
+        double divisao = num2 != 0 ? num1 / num2 : Double.NaN;//serve prar evitar a evitar divisao por zero
+        double resto = num1 % num2;
+        
+        //exibição dos resultados dados
+        
+        System.out.println("Resultado das operações:");
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisao: " + (num2 != 0 ? divisao : "Indefinida (divisisão por zero"));
+        System.out.println("Resto da divisão: " + resto);
+        
+        
     }
     
 }
